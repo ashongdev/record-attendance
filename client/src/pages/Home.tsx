612221f826ff74a810823d6e310030db1df853b2
ashongdev <ashongabdalla51@gmail.com>
2024-11-23 14:50:18 +0000

    Update files

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,28 +6,45 @@ const Home = () => {
 
 	return (
 		<main>
-			<div className="group cont">
-				<p>
-					Lecturers should use this button to register a course. Share the code to your
-					students to record their attendance.
-				</p>
-				<Link to="/lec/sign-in">
-					<button>Register Course</button>
-				</Link>
-			</div>
+			<fieldset>
+				<legend>Lecturer</legend>
 
-			<div className="group cont">
-				<p>Students should click on this button to check in into a course.</p>
-				<Link to="/std/check-in">
-					<button>Check In</button>
-				</Link>
-			</div>
-
-			{registered && (
-				<div>
-					<Link to="/lec/home">See Student List</Link>
+				<div className="group cont">
+					<p>
+						Lecturers should use this button to register a course. Share the code to
+						your students to record their attendance.
+					</p>
+					<Link to="/lec/register">
+						<button>Register Course</button>
+					</Link>
 				</div>
-			)}
+
+				<div className="group cont">
+					<p>Already registered a course? Click here to login.</p>
+					<Link to="/std/check-in">
+						<button>Log in</button>
+					</Link>
+				</div>
+
+				{/* {registered && ( */}
+				<Link
+					to="/lec/home"
+					className="cont"
+				>
+					<button className="home-link">View Student List</button>
+				</Link>
+				{/* // // )} */}
+			</fieldset>
+
+			<fieldset>
+				<legend>Students</legend>
+				<div className="group cont">
+					<p>Students should click on this button to check in into a course.</p>
+					<Link to="/std/check-in">
+						<button>Check In</button>
+					</Link>
+				</div>
+			</fieldset>
 		</main>
 	);
 };
