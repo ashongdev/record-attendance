accd95e9d445d73459d73a7e3939df11d2a23a95
ashongdev <ashongabdalla51@gmail.com>
2024-11-29 21:25:11 +0000

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,6 +2,10 @@ import { config } from "dotenv";
 import { Pool } from "pg";
 config();
 
+// export const pool = new Pool({
+// 	connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DBPORT}/${process.env.DATABASE}`,
+// });
+
 export const pool = new Pool({
-	connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DBPORT}/${process.env.DATABASE}`,
+	connectionString: process.env.DATABASE_URL,
 });
